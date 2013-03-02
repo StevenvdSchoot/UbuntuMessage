39e36aba122ee40cfb6bad121f1f7e386c140096
StevenvdSchoot <stevenvdschoot@gmail.com>
2013-03-02 10:41:01 +0100

    Got grid of the unused variable warning

--- cpphandler.h
+++ cpphandler.h
@@ -3,11 +3,13 @@
 
 #include <QObject>
 
+#define UNUSED(x) UNUSED_ ## x __attribute__((unused))
+
 class CppHandler : public QObject
 {
     Q_OBJECT
 public:
-    explicit CppHandler(QObject *parent = 0)
+    explicit CppHandler(QObject* UNUSED(parent) = 0)
     {
         ;
     }
